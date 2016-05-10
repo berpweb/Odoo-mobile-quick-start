dc50ddb837634dacf7e843e405824a047397cbe9
Kasim Rangwala <rangwalakasim@live.in>
2016-05-10 14:56:21 +0530

    Removed Frameworks Customer page. Added custom Partners page with RecyclerView, Added CardView

--- app/src/main/java/com/odoo/addons/partners/PartnersFragment.java
+++ app/src/main/java/com/odoo/addons/partners/PartnersFragment.java
@@ -90,7 +90,7 @@ public class PartnersFragment extends AppBaseFragment implements LoaderManager.L
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_partners, container, false);
+        return inflater.inflate(R.layout.commom_recyclerview, container, false);
     }
 
     @Override
